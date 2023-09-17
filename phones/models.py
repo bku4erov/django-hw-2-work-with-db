64ddaca26e445128e3763cfd66cba5b76da38b56
Boris Kucherov <boris.ku4erov@gmail.com>
2023-09-17 11:25:37 +0300

    add slugify phone name

--- phones/models.py
+++ phones/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from slugify import slugify
 
 
 class Phone(models.Model):
@@ -9,3 +10,7 @@ class Phone(models.Model):
     release_date = models.DateField()
     lte_exists = models.BooleanField()
     slug = models.SlugField()
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
